1400ae6be0f2386bc59976c511a8ac2e0c81c7ba
Otter <46843878+otter18@users.noreply.github.com>
2020-05-31 11:29:43 +0300

    read token from heroku VERN VAR

--- vk.py
+++ vk.py
@@ -6,6 +6,7 @@ from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.utils import get_random_id
 from vk_api import VkUpload
 from pd import *
+import os
 
 # Chat object for each unique user
 users = {}
@@ -15,12 +16,12 @@ def main():
     # img dowloading
     get_imgs()
     # auth
-    vk_session = vk_api.VkApi(token=open('config/token.txt', 'r').read())
+    # vk_session = vk_api.VkApi(token=open('config/token.txt', 'r').read())
+    vk_session = vk_api.VkApi(token=os.environ.get('VK_TOKEN', open('config/token.txt', 'r').read()))
     server = VkBotLongPoll(vk_session, 183487105)
 
     vk = vk_session.get_api()
     upload = VkUpload(vk_session)
-
 
     print('\033[01m', 'bot started...', '\033[0m')
     print('-' * 30)
@@ -41,7 +42,7 @@ def main():
             if bnt:
                 for row in bnt[:-1]:
                     for button in row:
-                        keyboard.add_button(button['txt'], color=button['color']) #NEGATIVE VkKeyboardColor.DEFAULT
+                        keyboard.add_button(button['txt'], color=button['color'])  # NEGATIVE VkKeyboardColor.DEFAULT
                     keyboard.add_line()
 
                 for button in bnt[-1]:
@@ -58,12 +59,12 @@ def main():
                 random_id=get_random_id(),
                 user_id=id,
                 keyboard=keyboard.get_keyboard() if bnt else keyboard.get_empty_keyboard(),
-                attachment= 'photo{}_{}'.format(photo['owner_id'], photo['id']) if url else None,
+                attachment='photo{}_{}'.format(photo['owner_id'], photo['id']) if url else None,
                 message=txt
             )
 
             print('\033[01m', 'Username:', '\033[0m', get_user_name_from_vk_id(id))
-            print('\033[01m', 'User msg:','\033[0m', msg)
+            print('\033[01m', 'User msg:', '\033[0m', msg)
             print('\033[01m', 'Bot msg:', '\033[0m', txt, url)
             print('-' * 30)
 
