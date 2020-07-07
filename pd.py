7d000c607aa990f76aedcd3daf015cd977825c0b
Otter <46843878+otter18@users.noreply.github.com>
2020-07-07 16:10:41 +0300

    code style

--- pd.py
+++ pd.py
@@ -6,12 +6,14 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 import bs4
 
+
 def get_table():
     url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSXOMQqsU8jk06Kre9A7QgRiAEw6GbLOWn0G75zzOMRbijlelTiuOMRHW43n6IMGtFlchdMjcR2uXAU/pub?output=csv'
     s = requests.get(url).content
     df = pd.read_csv(io.StringIO(s.decode('utf-8')))
 
     return df
+
 
 def get_imgs():
     df = get_table().to_dict()
@@ -57,6 +59,7 @@ def get_history(user_id):
     plt.savefig('img/%s.png' % user_id, dpi=100)
     plt.show()
 
+    
 def get_user_name_from_vk_id(user_id):
     request = requests.get("https://vk.com/id" + str(user_id))
     bs = bs4.BeautifulSoup(request.text, "html.parser")
@@ -81,7 +84,3 @@ def _clean_all_tag_from_str(string_line):
                 not_skip = True
 
     return result
-
-
-
-
